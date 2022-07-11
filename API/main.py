21a86afcd2c1ac564af0850fcf098f40837ca2c9
lucasnull <lucasnull@live.com>
2022-07-11 14:04:12 -0300

    test

--- API/main.py
+++ API/main.py
@@ -16,3 +16,6 @@ router.include_router(authors_controller.router)
 router.include_router(papers_controller.router)
 app.include_router(router)
 
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='127.0.0.1', port=8001, log_level="info", reload=True)
+    print("running")
